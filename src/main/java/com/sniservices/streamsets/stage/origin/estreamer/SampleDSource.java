9faebe76dde16bdb55e9d2eb6bc1a8b197ff661b
Shainnif Ismail <shaine@carsift.co>
2018-06-07 17:08:06 +0100

    moved back to the default icon

--- src/main/java/com/sniservices/streamsets/stage/origin/estreamer/SampleDSource.java
+++ src/main/java/com/sniservices/streamsets/stage/origin/estreamer/SampleDSource.java
@@ -29,7 +29,7 @@ import com.streamsets.pipeline.api.StageDef;
     version = 1,
     label = "Estreamer Origin",
     description = "connect to an estreamer service ",
-    icon = "cisco_logo.gif",
+    icon = "default.png",
     execution = ExecutionMode.STANDALONE,
     recordsByRef = true,
     onlineHelpRefUrl = ""
